c7fa80689cf6ebc0c9cdbc3f968fa059365e90ab
Hammad <hammad95632@gmail.com>
2026-03-09 19:36:58 +1100

    addition of module 2 + better comments

--- mod_1.py
+++ mod_1.py
@@ -13,7 +13,7 @@ model = HuggingFaceEndpoint(
 
 chat = ChatHuggingFace(llm=model, verbose=True)
 
-#simple chat with model
+#lesson 1 : simple chat with model
 messages = [
     ("system", "You are a helpful translator. Translate the user sentence to French."),
     ("human", "I love programming."),
@@ -24,7 +24,8 @@ messages = [
 
 #====================================================================================================
 
-#agent
+#Lesson 2: Creating an Agent
+
 from langchain.agents import create_agent
 from langchain.messages import HumanMessage, SystemMessage , AIMessage
 
@@ -40,7 +41,7 @@ print(response)
 
 #==================================================================================================== 
 
-#agent with system prompts
+#Lesson 3: Agent with System Prompts
 system_prompt = SystemMessage(content="You are a helpful assistant that answers questions about geography.")
 response = agent.invoke(
         {
@@ -51,7 +52,7 @@ print(response)
 
 #====================================================================================================
 
-#using tools
+#Lesson 4: Agent with Tools
 from langchain.tools import tool
 
 #can use tools to extend the capabilities of the agent. For example, we can define a tool to calculate the square of a number and use it in our agent. we can define the description and name within the decorator or in the function itself.
@@ -74,7 +75,7 @@ print(response)
 
 #====================================================================================================
 
-#agent with checkpointing
+#Lesson 5: Checkpointing and State Management
 from langgraph.checkpoint.memory import InMemorySaver
 agent_with_checkpoint = create_agent(model=chat, tools=[get_square1], checkpointer=InMemorySaver())
 
